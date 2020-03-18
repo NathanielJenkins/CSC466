b5e036609d4b76a9ee74c74fde1a8b5b0d95a197
lloucks <liamloucks@gmail.com>
2020-03-18 10:54:20 -0700

    added mode argument. 'python tor_visualization.py --resume true --mode dead_zones' will display the zones in which Tor could not connect to the webservers

--- tor_visualization.py
+++ tor_visualization.py
@@ -1,3 +1,5 @@
+import os.path
+from os import path
 import sys
 import time
 import os
@@ -49,6 +51,7 @@ def append_data(data, url, tor_req_time, elapsed_time, type, ip, rec):
     data['ip'].append(ip)
     data['geo'].append(rec)
     if rec is None:
+        data['rec'].append(rec)
         data['country'].append(None)
         data['country_code'].append(None)
         data['region'].append(None)
@@ -66,6 +69,7 @@ def append_data(data, url, tor_req_time, elapsed_time, type, ip, rec):
         print("rec.longitude:", rec.longitude)
         print("rec.zipcode:", rec.zipcode)
         print("rec.timezone:", rec.timezone)
+        data['rec'].append(rec)
         data['country'].append(rec.country_long)
         data['country_code'].append(rec.country_short)
         data['region'].append(rec.region)
@@ -80,7 +84,7 @@ def append_data(data, url, tor_req_time, elapsed_time, type, ip, rec):
 def make_requests(tor_session, reg_session, df, num_of_records) :
     database = IP2Location.IP2Location("IP2LOCATION-LITE-DB11.BIN")
 
-    data = {'ip': [], 'url' : [] , "time" : [], "elapsed_time" : [], "type": [], "geo": [], 
+    data = {'ip': [], 'url' : [] , "time" : [], "elapsed_time" : [], "type": [], "geo": [], "rec": [],
     "country": [], "country_code": [], "region": [], "city": [], "latitude": [], "longitude": [],
     "zipcode": [], "timezone": [] }
     count = 0
@@ -156,6 +160,85 @@ def extract(filename, data_type):
 
 
 
+def get_c_codes():
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
+    print("df")
+    print(df)
+    print("df[df['COUNTRY'].str.startswith('U')")
+    print(df[df['COUNTRY'].str.startswith("U")])
+    c_codes_df = df.drop_duplicates(subset=['COUNTRY','CODE'])
+    c_codes_df = c_codes_df[['COUNTRY','CODE']]
+    #c_codes_df = df.groupby('CODE')[['COUNTRY', 'CODE']].count()
+    c_codes_df = c_codes_df.rename(columns={"COUNTRY": "country", "CODE": "country_code"})
+    c_codes_df.to_csv('c_codes_df.csv')
+    print("dc_codes_df:", c_codes_df )
+
+
+def dead_zones_viz(df):
+
+    index_cols = ['rec', 'country_code', 'elapsed_time', 'country']
+    map_df = df[index_cols]
+    map_df.dropna(subset=['rec'])
+    dead_zones_df = map_df[map_df['elapsed_time'].isna()]
+    dead_zones_df['country_code'] = dead_zones_df['country'].str[:3].str.upper()
+    dead_zones_df.to_csv('dead_zones_df.csv')
+    map_df['count'] = map_df.groupby('country_code')['country_code'].transform('count')
+    z_values = pd.Series( (map_df['count']) )
+    BEL_Total = dead_zones_df[dead_zones_df.country_code == "BEL"].size
+    grouped_counts = dead_zones_df.groupby(['country']).country_code.count()
+    grouped_df = dead_zones_df.groupby(['country'])
+
+    if not path.exists('c_codes_df.csv'):
+        get_c_codes()
+    c_codes_df = pd.read_csv('c_codes_df.csv') 
+    c_unique = c_codes_df.drop_duplicates(subset=['country_code', "country"])
+    df_unique = df.drop_duplicates(subset=['country_code', "country"])
+    code_dict = {}
+    for index, row in c_unique.iterrows():
+        country = row.country
+        df_code = country[:3].upper()
+        c_code = row.country_code
+        code_dict[df_code] = row.country_code
+
+  
+    dead_zones_df = dead_zones_df.rename(columns={"country_code": "code"})
+    dead_zones_df['code'] = dead_zones_df['code'].map(code_dict)
+
+    fig = go.Figure(data=go.Choropleth(
+        locations = dead_zones_df['code'],
+        z = z_values,
+        text = dead_zones_df['country'],
+        colorscale = 'Reds',
+        autocolorscale=False,
+        reversescale=False,
+        marker_line_color='darkgray',
+        marker_line_width=0.5,
+        colorbar_tickprefix = '',
+        colorbar_title = 'Tor Dead Zones<br>No Tor Connection',
+    ))
+
+    fig.update_layout(
+        title_text='Tor Dead Zones',
+        geo=dict(
+            showframe=False,
+            showcoastlines=False,
+            projection_type='equirectangular'
+        ),
+        annotations = [dict(
+            x=0.55,
+            y=0.1,
+            xref='paper',
+            yref='paper',
+            text='Tor: <a href="https://www.torproject.org/">\
+                The Tor Project</a>',
+            showarrow = False
+        )]
+    )
+
+    return fig
+
+
+
 
 def map_viz(df):
     index_cols = ['country_code', 'elapsed_time', 'country']
@@ -217,11 +300,12 @@ def read_data(filename):
 def handle_args():
     usage_err_msg = """\n ARGUMENTS ERROR \n\n 
     usage:  \n 
-    'tor_visualization.py --resume true' will load data from previous run and visualize the data on the world map \n \n  
-    'tor_visualization.py --test_size 100' will run the tor network speed tests on 100 different webservers \n \n 
-    'tor_visualization.py --save true' will save the graph to a file \n \n 
-    'tor_visualization.py --outfile mygraph.png' will save the graph to a file called  mygraph.png  \n \n 
-    'tor_visualization.py --display false will prevent the visualization from being hosted on localhost \n \n 
+    'python tor_visualization.py --resume true' will load data from previous run and visualize the data on the world map \n \n  
+    'python tor_visualization.py --test_size 100' will run the tor network speed tests on 100 different webservers \n \n 
+    'python tor_visualization.py --save true' will save the graph to a file \n \n 
+    'python tor_visualization.py --outfile mygraph.png' will save the graph to a file called  mygraph.png  \n \n 
+    'python tor_visualization.py --display false' will prevent the visualization from being hosted on localhost \n \n 
+    'python tor_visualization.py --resume true --mode dead_zones' will display the zones where the Tor network could not connect to a server \n \n
     """ 
     arguments = len(sys.argv) - 1
     print("arguments:", arguments)
@@ -244,11 +328,12 @@ def handle_args():
 
 '''
 usage:   
-    'tor_visualization.py --resume true' will load data from previous run and visualize the data on the world map 
-    'tor_visualization.py --test_size 100' will run the tor network speed tests on 100 different webservers 
-    'tor_visualization.py --save true' will save the graph to a file 
-    'tor_visualization.py --outfile mygraph.png' will save the graph to a file called  mygraph.png 
-    'tor_visualization.py --display false will prevent the visualization from being hosted on localhost 
+    'python tor_visualization.py --resume true' will load data from previous run and visualize the data on the world map 
+    'python tor_visualization.py --test_size 100' will run the tor network speed tests on 100 different webservers 
+    'python tor_visualization.py --save true' will save the graph to a file 
+    'python tor_visualization.py --outfile mygraph.png' will save the graph to a file called  mygraph.png 
+    'python tor_visualization.py --display false will prevent the visualization from being hosted on localhost 
+    'python tor_visualization.py --resume true --mode dead_zones'
 '''
 
 if __name__ == '__main__':
@@ -259,10 +344,14 @@ if __name__ == '__main__':
     resume = False
     test_size = False
     size = 50
+
+
+
     if '--resume' in arg_dict:
         if(arg_dict['--resume'] == 'true'):
             resume = True
             df = pd.read_csv(output_filename) 
+
     if '--test_size' in arg_dict:
         if(arg_dict['--test_size'].isdigit()):
             test_size = True
@@ -277,9 +366,13 @@ if __name__ == '__main__':
         df.to_csv(output_filename)
 
 
-
-
     fig = map_viz(df)
+    if '--mode' in arg_dict:
+        if(arg_dict['--mode'] == 'dead_zones'):
+            fig = dead_zones_viz(df)
+
+            
+
     display = True
     save = False
     outfile = "tor_map.png"
@@ -298,9 +391,12 @@ if __name__ == '__main__':
     if display==True:
         fig.show()
 
-    print("outfile:", outfile)
+
     if save==True:
         fig.write_image(outfile)
+
+
+
 
     
 
