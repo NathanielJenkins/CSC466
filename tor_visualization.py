fe0c3949cf7e723cac01729ba412cfb432ec3667
Liam Loucks <liamloucks@gmail.com>
2020-03-18 19:59:32 -0700

    created dead_zone_ratios_viz(df) which maps the ratio of dropped connections in tor per region

--- tor_visualization.py
+++ tor_visualization.py
@@ -141,9 +141,6 @@ def get_data():
     extract(db_filename, "BIN")
     bin_db_filename = db_filename+".BIN"
     df = read_data(csv_filename)
-    print("df:", df)
-    print("list(df.columns.values):", list(df.columns.values))
-    print(df["Domain"])
     return df
 
 def extract(filename, data_type):
@@ -167,20 +164,83 @@ def get_c_codes():
     c_codes_filename =  Path.cwd().joinpath(data_dir, c_codes_file)
     if not path.exists(c_codes_filename.absolute()):
         df = read_data('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv', False)
-        print("df")
-        print(df)
-        print("df[df['COUNTRY'].str.startswith('U')")
-        print(df[df['COUNTRY'].str.startswith("U")])
         c_codes_df = df.drop_duplicates(subset=['COUNTRY','CODE'])
         c_codes_df = c_codes_df[['COUNTRY','CODE']]
-        #c_codes_df = df.groupby('CODE')[['COUNTRY', 'CODE']].count()
         c_codes_df = c_codes_df.rename(columns={"COUNTRY": "country", "CODE": "country_code"})
         write_csv(c_codes_df, 'c_codes_df.csv')
-        print("dc_codes_df:", c_codes_df )
     else:
         c_codes_df = read_data(c_codes_file) 
 
     return c_codes_df
+
+
+def dead_zone_ratios_viz(df):
+
+    index_cols = ['rec', 'country_code', 'elapsed_time', 'country']
+    map_df = df[index_cols]
+    map_df.dropna(subset=['rec'])
+    dead_zones_df = map_df[map_df['elapsed_time'].isna()]
+    dead_zones_df['country_code'] = dead_zones_df['country'].str[:3].str.upper()
+    write_csv(dead_zones_df, 'dead_zones_df.csv')
+    dead_zones_counts = dead_zones_df.groupby(['country']).country_code.count()
+    map_counts = map_df.groupby(['country']).country_code.count()
+    
+    dead_zones_dict = dead_zones_counts.to_dict()
+    for country in map_counts.to_dict().keys():
+        if country not in dead_zones_counts.to_dict().keys():
+            print("country:", country)
+            dead_zones_dict[country] = 1
+
+    dead_zones_counts = pd.Series(dead_zones_dict)
+    map_df['count'] = map_df.groupby('country_code')['country_code'].transform('count')
+    z_values = map_counts/dead_zones_counts
+
+    c_codes_df = get_c_codes()
+    c_unique = c_codes_df.drop_duplicates(subset=['country_code', "country"])
+    code_dict = {}
+    for index, row in c_unique.iterrows():
+        country = row.country
+        df_code = country[:3].upper()
+        code_dict[df_code] = row.country_code
+
+    dead_zones_df = dead_zones_df.rename(columns={"country_code": "code"})
+    dead_zones_df['code'] = dead_zones_df['code'].map(code_dict)
+
+    fig = go.Figure(data=go.Choropleth(
+        locations = dead_zones_df['code'],
+        z = z_values,
+        text = dead_zones_df['country'],
+        colorscale = 'Reds',
+        autocolorscale=False,
+        reversescale=False,
+        marker_line_color='darkgray',
+        marker_line_width=0.5,
+        colorbar_tickprefix = '',
+        colorbar_title = 'Tor Dead Ratio',
+    ))
+
+    fig.update_layout(
+        title_text='Tor Dead Zone Ratio',
+        geo=dict(
+            showframe=False,
+            showcoastlines=False,
+            projection_type='equirectangular'
+        ),
+        annotations = [dict(
+            x=0.55,
+            y=0.1,
+            xref='paper',
+            yref='paper',
+            text='Tor: <a href="https://www.torproject.org/">\
+                The Tor Project</a>',
+            showarrow = False
+        )]
+    )
+
+    return fig
+
+
+
 
 def dead_zones_viz(df):
 
@@ -192,14 +252,9 @@ def dead_zones_viz(df):
     write_csv(dead_zones_df, 'dead_zones_df.csv')
     map_df['count'] = map_df.groupby('country_code')['country_code'].transform('count')
     z_values = pd.Series( (map_df['count']) )
-    #BEL_Total = dead_zones_df[dead_zones_df.country_code == "BEL"].size
-    #grouped_counts = dead_zones_df.groupby(['country']).country_code.count()
-    #grouped_df = dead_zones_df.groupby(['country'])
-
 
     c_codes_df = get_c_codes()
     c_unique = c_codes_df.drop_duplicates(subset=['country_code', "country"])
-    #df_unique = df.drop_duplicates(subset=['country_code', "country"])
     code_dict = {}
     for index, row in c_unique.iterrows():
         country = row.country
@@ -255,23 +310,16 @@ def map_viz(df):
     #mean = df["elapsed_time"].mean()
     #std =  df["elapsed_time"].std()
 
-
     map_df = map_df[map_df['elapsed_time'] < map_df['elapsed_time'].quantile(.80)]
-    print("map_df")
-    print(map_df)
-    print("map_df['elapsed_time']")
-    print(map_df['elapsed_time'])
 
     c_codes_df = get_c_codes()
     c_unique = c_codes_df.drop_duplicates(subset=['country_code', "country"])
-    #df_unique = df.drop_duplicates(subset=['country_code', "country"])
     code_dict = {}
     for index, row in c_unique.iterrows():
         country = row.country
         df_code = country[:3].upper()
         code_dict[df_code] = row.country_code
 
-  
 
     map_df['country_code'] = map_df['country_code'].map(code_dict)
 
@@ -344,6 +392,7 @@ def handle_args():
     'python tor_visualization.py --outfile mygraph.png' will save the graph to a file called  mygraph.png  \n \n 
     'python tor_visualization.py --display false' will prevent the visualization from being hosted on localhost \n \n 
     'python tor_visualization.py --resume true --mode dead_zones' will display the zones where the Tor network could not connect to a server \n \n
+    'python tor_visualization.py --resume true --mode dead_zone_ratios' will display the ratio of connections to dropped connections per region\n \n
     """ 
     arguments = len(sys.argv) - 1
     print("arguments:", arguments)
@@ -372,6 +421,7 @@ usage:
     'python tor_visualization.py --outfile mygraph.png' will save the graph to a file called  mygraph.png 
     'python tor_visualization.py --display false will prevent the visualization from being hosted on localhost 
     'python tor_visualization.py --resume true --mode dead_zones'
+    'python tor_visualization.py --resume true --mode dead_zone_ratios' will display the ratio of connections to dropped connections per region
 '''
 
 if __name__ == '__main__':
@@ -383,7 +433,8 @@ if __name__ == '__main__':
     test_size = False
     size = 50
 
-
+    df = read_data(output_filename) 
+    dead_zone_ratios_viz(df)
 
     if '--resume' in arg_dict:
         if(arg_dict['--resume'] == 'true'):
@@ -403,13 +454,12 @@ if __name__ == '__main__':
         print(df.columns)
         write_csv(df, output_filename)
 
-
     fig = map_viz(df)
     if '--mode' in arg_dict:
         if(arg_dict['--mode'] == 'dead_zones'):
             fig = dead_zones_viz(df)
-
-            
+        if(arg_dict['--mode'] == 'dead_zone_ratios'):
+            fig = dead_zone_ratios_viz(df)
 
     display = True
     save = False
@@ -428,7 +478,6 @@ if __name__ == '__main__':
 
     if display==True:
         fig.show()
-
 
     if save==True:
         write_image(fig, outfile)
